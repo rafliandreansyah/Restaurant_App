b8fce737bd85964dd58423131bede25222e05938
rafli <rafli@mediainovasi.id>
2023-03-29 15:24:16 +0700

    add widget card

--- lib/screen/restaurants_screen.dart
+++ lib/screen/restaurants_screen.dart
@@ -1,13 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/provider/restaurant_provider.dart';
 import 'package:restaurant_app/screen/restaurant_detail_screen.dart';
-import 'package:restaurant_app/style/theme.dart';
-
-import '../data/model/restaurant_detail.dart';
+import 'package:restaurant_app/widget/restaurant_card.dart';
 
 class RestaurantsScreen extends StatefulWidget {
   const RestaurantsScreen({Key? key}) : super(key: key);
@@ -75,114 +70,12 @@ class _RestaurantsScreenState extends State<RestaurantsScreen> {
                           shrinkWrap: true,
                           itemCount: state.listRestaurant.length,
                           itemBuilder: (context, index) {
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(
-                                    context, RestaurantDetailScreen.routeName,
-                                    arguments: state.listRestaurant[index]);
-                              },
-                              child: SizedBox(
-                                height: 300,
-                                child: Card(
-                                  elevation: 4,
-                                  margin:
-                                      const EdgeInsets.symmetric(vertical: 8),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(16),
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(
-                                        width: double.infinity,
-                                        height: 150,
-                                        child: ClipRRect(
-                                          borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(21),
-                                            topRight: Radius.circular(21),
-                                          ),
-                                          child: Image.network(
-                                            state.listRestaurant[index]
-                                                        .pictureId !=
-                                                    null
-                                                ? 'https://restaurant-api.dicoding.dev/images/small/${state.listRestaurant[index].pictureId}'
-                                                : '',
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                          horizontal: 16,
-                                          vertical: 16,
-                                        ),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              state.listRestaurant[index]
-                                                      .name ??
-                                                  '',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .headline6,
-                                              maxLines: 2,
-                                            ),
-                                            const SizedBox(
-                                              height: 8,
-                                            ),
-                                            Row(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.end,
-                                              children: [
-                                                const Icon(
-                                                  Icons.location_city,
-                                                ),
-                                                const SizedBox(
-                                                  width: 8,
-                                                ),
-                                                Text(
-                                                  state.listRestaurant[index]
-                                                          .city ??
-                                                      '',
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .subtitle2,
-                                                ),
-                                              ],
-                                            ),
-                                            const SizedBox(
-                                              height: 8,
-                                            ),
-                                            Row(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.end,
-                                              children: [
-                                                const Icon(
-                                                  Icons.star,
-                                                  color: colorYellow,
-                                                ),
-                                                const SizedBox(
-                                                  width: 8,
-                                                ),
-                                                Text(
-                                                  state.listRestaurant[index]
-                                                      .rating
-                                                      .toString(),
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .subtitle2,
-                                                ),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
+                            return RestaurantCard(
+                              restaurant: state.listRestaurant[index],
+                              onClick: () => Navigator.pushNamed(
+                                context,
+                                RestaurantDetailScreen.routeName,
+                                arguments: state.listRestaurant,
                               ),
                             );
                           },
---
+++ lib/widget/restaurant_card.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/data/model/restaurant.dart';
+
+import '../style/theme.dart';
+
+class RestaurantCard extends StatelessWidget {
+  final Restaurant restaurant;
+  final Function() onClick;
+  const RestaurantCard({Key? key, required this.restaurant, required this.onClick}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onClick,
+      child: SizedBox(
+        height: 300,
+        child: Card(
+          elevation: 4,
+          margin:
+          const EdgeInsets.symmetric(vertical: 8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment:
+            CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 150,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(21),
+                    topRight: Radius.circular(21),
+                  ),
+                  child: Image.network(
+                    restaurant
+                        .pictureId !=
+                        null
+                        ? 'https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}'
+                        : '',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment:
+                  CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      restaurant
+                          .name ??
+                          '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6,
+                      maxLines: 2,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      crossAxisAlignment:
+                      CrossAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.location_city,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          restaurant
+                              .city ??
+                              '',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle2,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      crossAxisAlignment:
+                      CrossAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          color: colorYellow,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          restaurant
+                              .rating
+                              .toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle2,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
